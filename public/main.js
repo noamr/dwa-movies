69cd0bbeb325bc770b8ae08ad26c532dae224291
Noam Rosenthal <nrosenthal@google.com>
2026-01-15 12:54:10 +0000

    Lots of changes

--- public/main.js
+++ public/main.js
@@ -1,25 +1,10 @@
-document.documentElement.dataset.supported = ("patchAll" in document) && ("routeMap" in document);
+document.documentElement.dataset.supported = ("streamAppendHTMLUnsafe" in document.body);
 
 const worker = new Worker("/worker.js", {type: "module"});
-let current_navigation = Promise.resolve();
-let did_snap_by_user = false;
-/**
- *
- * @param {Request} request
- */
 async function patch_navigation(url) {
-    const {promise, resolve} = Promise.withResolvers();
-    current_navigation = promise;
     const {readable, writable} = new TransformStream();
-    worker.postMessage({request: {
-        url
-    }, stream: writable}, [writable]);
-    await readable.pipeTo(document.patchAll());
-    for (const item of document.querySelectorAll(".default-item"))
-        item.parentElement.scrollLeft = item.offsetLeft;
-
-    did_snap_by_user = false;
-    resolve();
+    worker.postMessage({url, stream: writable}, [writable]);
+    await readable.pipeTo(document.body.streamAppendHTMLUnsafe());
 }
 
 await new Promise(resolve => worker.addEventListener("message", () => {
@@ -29,58 +14,21 @@ await new Promise(resolve => worker.addEventListener("message", () => {
 patch_navigation(location.href);
 
 document.addEventListener("scrollsnapchange", e => {
-    if (!did_snap_by_user) {
-        did_snap_by_user = true;
-        return;
-    }
     const snapped = e.snapTargetInline.querySelector(".snap-to-activate");
     if (snapped)
         snapped.click();
 }, {capture: true})
 
 window.navigation.addEventListener("navigate", e => {
-    const from = location.href;
-    const links_to_cleanup = new Set();
     if (e.canIntercept) {
-        if (e.sourceElement) {
-            e.sourceElement.classList.add("nav-trigger");
-            links_to_cleanup.add(e.sourceElement);
-        }
-        for (const a of document.querySelectorAll("a[href]")) {
-            if (a.href === e.destination.url) {
-                a.classList.add("nav-dest");
-                a.classList.add("nav");
-                links_to_cleanup.add(a);
-            }
-        }
         e.intercept({
-            precommitHandler() {
-                const {promise, resolve} = Promise.withResolvers();
-                const transition = document.startViewTransition(() => {
-                    resolve();
-                    return navigation.transition.finished;
-                });
-                Promise.allSettled([transition.finished]).then(() => {
-                    for (const a of links_to_cleanup) {
-                        a.classList.remove("nav");
-                        a.classList.remove("nav-source");
-                        a.classList.remove("nav-dest");
-                        a.classList.remove("nav-trigger");
-                    }
-                });
-                return promise;
-            },
-
             async handler() {
-                await patch_navigation(e.destination.url);
-                for (const a of document.querySelectorAll("a[href]")) {
-                    if (a.href === from) {
-                        a.classList.add("nav-dest");
-                        a.classList.add("nav");
-                        links_to_cleanup.add(a);
-                        break;
-                    }
-                }
+                await new Promise(resolve => requestAnimationFrame(resolve));
+                const {promise, resolve} = Promise.withResolvers();
+                return document.startViewTransition(async () => {
+                    resolve();
+                    await patch_navigation(e.destination.url)
+                }).finished;
             }
         })
     }
--- public/worker.js
+++ public/worker.js
@@ -1,12 +1,12 @@
 const csrf_token_promise = (async () => {
     const session_res = await fetch("/api/session");
-    const {csrfToken} = await session_res.json();
+    const { csrfToken } = await session_res.json();
     return csrfToken;
 })();
 
 async function tmdb_get(path) {
     const response = await fetch('/api/tmdb-proxy', {
-        headers: {"x-csrf-token": await csrf_token_promise, "x-tmdb-path": path }
+        headers: { "x-csrf-token": await csrf_token_promise, "x-tmdb-path": path }
     });
     return response.json();
 }
@@ -27,17 +27,23 @@ console.log("Worker ready");
 postMessage("ready");
 
 self.addEventListener("message", async e => {
-    const {request, stream} = e.data;
-    const writer = stream.getWriter();
-    const url = new URL(request.url);
+    const writer = e.data.stream.getWriter();
+    const url = new URL(e.data.url);
     const all = [];
 
+    function write_patch(targetTagname, targetName, content) {
+        return writer.write(`
+            <template contentmethod="replace-children"><${targetTagname} contentname="${targetName}">
+                ${content}
+            </${targetTagname}></template>
+        `);
+    }
+
     function get_movie_list(url, outlet) {
-        return tmdb_get(url).then(async ({results, success}) => {
-           await writer.write(`
-      <template patchfor="list-${outlet}">
+        return tmdb_get(url).then(async ({ results, success }) => {
+            await write_patch("div", `list-${outlet}`, `
          <ul class=movie-list>
-        ${results.map(({id, poster_path, title}) => `
+        ${results.map(({ id, poster_path, title }) => `
             <li>
                 <a href="/movie/${id}?list=${encodeURIComponent(url)}" class="movie-thumb">
                     <img class=thumb src="${image_path(poster_path, 200)}">
@@ -46,230 +52,205 @@ self.addEventListener("message", async e => {
             </li>
         `).join("")}
         </ul>
-  </template>
     `
-);
+            );
         });
     }
     if (new URLPattern("/", url.href).test(url)) {
-        writer.write(`
-  <template patchfor="main">
+        write_patch("main", "main", `
     <section class=movies>
       <h2>Now Playing</h2>
-      <div id="list-now_playing"></div>
+      <div contentname="list-now_playing"></div>
     </section>
     <section class=movies>
       <h2>Popular</h2>
-      <div id="list-popular"></div>
+      <div contentname="list-popular"></div>
     </section>
     <section class=movies>
       <h2>Top Rated</h2>
-      <div id="list-top_rated"></div>
+      <div contentname="list-top_rated"></div>
     </section>
     <section class=movies>
       <h2>Upcoming</h2>
-      <div id="list-upcoming"></div>
+      <div contentname="list-upcoming"></div>
     </section>
-    <section id=genres>
+    <section contentname=genres>
     </section>
-  </template>
         `);
-    for (const list of ["top_rated", "popular", "upcoming", "now_playing"]) {
-        all.push(get_movie_list(`/movie/${list}`, list));
+        for (const list of ["top_rated", "popular", "upcoming", "now_playing"]) {
+            all.push(get_movie_list(`/movie/${list}`, list));
+        }
+
+        all.push(tmdb_get("/genre/movie/list").then(async ({ genres }) => {
+            write_patch("section", "genres", `
+                ${genres.map(({ id, name }) => `<section class=movies contentname=genre-${id}>
+                    <h2>${name}</h2>
+                    <div contentname="list-genre-${id}"></div>
+                </section>`).join("")}
+            `);
+
+            await Promise.all(genres.map(async ({ id }) => get_movie_list(`/discover/movie?with_genres=${id}`, `genre-${id}`)))
+        }));
+
     }
 
-    all.push(tmdb_get("/genre/movie/list").then(async ({genres}) => {
-        await writer.write(`
-            <template patchfor=genres>
-                ${genres.map(({id, name}) => `<section class=movies id=genre-${id}>
-                    <h2>${name}</h2>
-                    <div id="list-genre-${id}"></div>
-                </section>`).join("")}
-            </template>
-        `);
-
-        await Promise.all(genres.map(async ({id}) => get_movie_list(`/discover/movie?with_genres=${id}`, `genre-${id}`)))
-    }));
-
-}
-
-const current_movie = new URLPattern("/movie/:id", url.href).exec(url)?.pathname?.groups?.id;
-if (current_movie) {
-    await tmdb_get(`/movie/${current_movie}`).then(async ({
-        backdrop_path,
-        genres,
-        homepage,
-        imdb_id,
-        title,
-        overview,
-        poster_path
-    }) => {
-        await writer.write(`
-    <template patchfor="main">
+    const current_movie = new URLPattern("/movie/:id", url.href).exec(url)?.pathname?.groups?.id;
+    if (current_movie) {
+        await tmdb_get(`/movie/${current_movie}`).then(async ({
+            backdrop_path,
+            genres,
+            homepage,
+            imdb_id,
+            title,
+            overview,
+            poster_path
+        }) => {
+            write_patch("main", "main", `
         <ul class="movie-carousel">
-        <li id="prev-movie"></li>
+        <li contentname="prev-movie"></li>
             <li class="default-item">
                 <article class="movie-details">
+                    <img class="backdrop" src="${image_path(backdrop_path, 1280)}" />
                     <h1>${title}</h1>
-                    <img class="hero" src="${image_path(poster_path, 300)}" width="300" />
+                    <img class="hero" src="${image_path(poster_path, 300)}" data-poster-for="movie-${current_movie}" width="300" />
                     <p class="overview">${overview}</p>
-                    <section id="cast">
+                    <section contentname="cast">
                     </section>
                     <section class=movies>
                     <h2>Related</h2>
-                    <div id="list-similar"></div>
+                    <div contentname="list-similar"></div>
                     </section>
                     <section class=movies>
                     <h2>Recommended</h2>
-                    <div id="list-recommendations"></div>
+                    <div contentname="list-recommendations"></div>
                     </section>
                 </article>
             </li>
-            <li id="next-movie"></li>
+            <li contentname="next-movie"></li>
         </ul>
-    </template>
-    `) });
+    `)
+        });
 
 
-   all.push(tmdb_get(`/movie/${current_movie}/credits`).then(({cast}) => {
-    writer.write(`
-    <template patchfor="cast">
+        all.push(tmdb_get(`/movie/${current_movie}/credits`).then(({ cast }) => {
+            write_patch("section", "cast", `
         <ul class=cast>
-            ${cast.map(({id, name, character, profile_path}) => `
+            ${cast.map(({ id, name, character, profile_path }) => `
                 <li class=cast>
                     <a href="/person/${id}?list=${encodeURIComponent(`/movie/${current_movie}/credits`)}">
-                        <img class="person thumb" src="${image_path(profile_path, 300)}" width=75>
+                        <img class="person thumb" data-poster-for="person-${id}" src="${image_path(profile_path, 300)}" width=80 height=120>
                         <span>${name}</span> as <span>${character}</span>
                     </a>
                 </li>
             `).join("")}
         </ul>
-    </template>
-        `);
-   }));
+    `);
+        }));
 
 
-    const current_list = url.searchParams.get("list");
-    if (current_list) {
-        function movie_slide({id,title, poster_path, overview}) {
-            return `<article class="movie-details">
+        const current_list = url.searchParams.get("list");
+        if (current_list) {
+            function movie_slide({ id, title, poster_path, overview, backdrop_path }) {
+                return `<article class="movie-details">
+                        <img class="backdrop" src="${image_path(backdrop_path, 1280)}" hidden>
                         <h1>${title}</h1>
-                        <img class="hero" src="${image_path(poster_path, 300)}" width="300">
+                        <img class="hero" src="${image_path(poster_path, 300)}" data-poster-for="movie-${id}" width="300">
                         <p class="overview">${overview}</p>
                         <a href="/movie/${id}?list=${current_list}" class="snap-to-activate">&nbsp;</a>
                     </article>
             `;
+            }
+            console.log(current_list)
+            all.push(tmdb_get(current_list).then(async (data) => {
+                const results = data.results || data.cast;
+                const index = results.findIndex(r => r.id == current_movie);
+                if (index < results.length - 1) {
+                    const next = results[index + 1];
+                    console.log({ next })
+                    write_patch("li", "next-movie", movie_slide(next));
+                }
+                if (index > 0) {
+                    const prev = results[index - 1];
+                    write_patch("li", "prev-movie", movie_slide(prev));
+                }
+            }));
         }
-        all.push(tmdb_get(current_list).then(async ({results}) => {
-            const index = results.findIndex(r => r.id == current_movie);
-            if (index < results.length - 1) {
-                const next = results[index + 1];
-                console.log({next})
-                await writer.write(`
-                    <template patchfor="next-movie">
-                        ${movie_slide(next)}
-                    </template>
-                `);
-            }
-            if (index > 0) {
-                const prev = results[index - 1];
-                await writer.write(`
-                    <template patchfor="prev-movie">
-                        ${movie_slide(prev)}
-                    </template>
-                `);
-            }
-        }));
+        all.push(get_movie_list(`/movie/${current_movie}/similar`, "similar"));
+        all.push(get_movie_list(`/movie/${current_movie}/recommendations`, "recommendations"));
     }
-    all.push(get_movie_list(`/movie/${current_movie}/similar`, "similar"));
-    all.push(get_movie_list(`/movie/${current_movie}/recommendations`, "recommendations"));
-}
 
-const current_person = new URLPattern("/person/:id", url.href).exec(url)?.pathname?.groups?.id;
-if (current_person) {
-    const {
-        name,
-        biography,
-        profile_path
-    } = await tmdb_get(`/person/${current_person}`);
+    const current_person = new URLPattern("/person/:id", url.href).exec(url)?.pathname?.groups?.id;
+    if (current_person) {
+        const {
+            name,
+            biography,
+            profile_path
+        } = await tmdb_get(`/person/${current_person}`);
 
-    await writer.write(`
-    <template patchfor="main">
+        write_patch("main", "main", `
         <ul class="person-carousel">
-        <li id="prev-person"></li>
+            <li contentname="prev-person"></li>
             <li class="default-item">
                 <article class="person-details">
                     <h1>${name}</h1>
-                    <img class="hero" src="${image_path(profile_path, 300)}" width="300" />
+                    <img class="hero" src="${image_path(profile_path, 300)}" data-poster-for="person-${current_person}" width="300">
                     <p class="overview">${biography}</p>
-                    <section id="cast">
+                    <section contentname="cast" class=mini-carousel>
                     </section>
                     <section class=movies>
                     <h2>Credits</h2>
-                    <div id="credits"></div>
+                    <div contentname="credits"></div>
                     </section>
                 </article>
             </li>
-            <li id="next-person"></li>
+            <li contentname="next-person"></li>
         </ul>
-    </template>
     `);
 
 
-   all.push(tmdb_get(`/person/${current_person}/credits`).then(async ({cast}) => {
-    await writer.write(`
-    <template patchfor="cast">
+        all.push(tmdb_get(`/person/${current_person}/credits`).then(async ({ cast }) => {
+            write_patch("section", "cast", `
         <ul class=cast>
-            ${cast.map(({title, poster_path, id, character}) => `
+            ${cast.map(({ title, poster_path, id, character }) => `
                 <li class=cast>
-                    <a href="/movie/${id}">
-                        <img class="movie thumb" src="${image_path(poster_path, 300)}" width=75>
+                    <a href="/movie/${id}?list=/person/${current_person}/credits">
+                        <img class="movie thumb" data-poster-for="movie-${id}" src="${image_path(poster_path, 300)}" width=80 height=120>
                         <span>As ${character}</span> in <span>${title}</span>
                     </a>
                 </li>
             `).join("")}
         </ul>
-    </template>
-        `);
-   }));
+    `);
+        }));
 
 
-    const current_list = url.searchParams.get("list");
-    if (current_list) {
-        function person_slide({id, name, profile_path}) {
-            console.log
-            return `<article class="person-details">
+        const current_list = url.searchParams.get("list");
+        if (current_list) {
+            function person_slide({ id, name, profile_path }) {
+                return `<article class="person-details">
                         <h1>${name}</h1>
-                        <img class="hero" src="${image_path(profile_path, 300)}" width="300">
+                        <img class="hero" src="${image_path(profile_path, 300)}" data-poster-for="person-${id}" width="300">
                         <a href="/person/${id}?list=${current_list}" class="snap-to-activate">&nbsp;</a>
                     </article>
             `;
+            }
+            all.push(tmdb_get(current_list).then(async ({ cast }) => {
+                const results = cast;
+                const index = results.findIndex(r => r.id == current_person);
+                if (index < results.length - 1) {
+                    const next = results[index + 1];
+                    console.log({ next })
+                    write_patch("li", "next-person", person_slide(next));
+                }
+                if (index > 0) {
+                    const prev = results[index - 1];
+                    write_patch("li", "prev-person", person_slide(prev));
+                }
+            }));
         }
-        all.push(tmdb_get(current_list).then(async ({cast}) => {
-            const results = cast;
-            const index = results.findIndex(r => r.id == current_person);
-            if (index < results.length - 1) {
-                const next = results[index + 1];
-                console.log({next})
-                await writer.write(`
-                    <template patchfor="next-person">
-                        ${person_slide(next)}
-                    </template>
-                `);
-            }
-            if (index > 0) {
-                const prev = results[index - 1];
-                await writer.write(`
-                    <template patchfor="prev-person">
-                        ${person_slide(prev)}
-                    </template>
-                `);
-            }
-        }));
     }
-}
 
-Promise.all(all).then(() => {
-    writer.close();
-});
+    Promise.all(all).then(() => {
+        writer.close();
+    });
 });
